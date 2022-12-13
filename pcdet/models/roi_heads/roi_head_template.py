ab85de3a09be7a20bad3362c3f38aa316d03524d
shashankag14 <shashankag14@gmail.com>
2022-12-13 14:14:20 +0100

    DA-7 Adding preloss filtering for labeled samples for reg_valid_mask
    This commit is only for testing purpose

--- pcdet/models/roi_heads/roi_head_template.py
+++ pcdet/models/roi_heads/roi_head_template.py
@@ -421,22 +421,22 @@ class RoIHeadTemplate(nn.Module):
         unlabeled_inds = self.forward_ret_dict['unlabeled_inds']
         rcnn_cls_labels = self.forward_ret_dict['rcnn_cls_labels'].clone().detach()
         rcnn_cls_preds = self.forward_ret_dict['rcnn_cls'].view_as(rcnn_cls_labels).clone().detach()
-        rcnn_cls_labels = rcnn_cls_labels[unlabeled_inds]
-        rcnn_cls_preds = torch.sigmoid(rcnn_cls_preds)[unlabeled_inds]
+        rcnn_cls_labels_ul = rcnn_cls_labels[unlabeled_inds]
+        rcnn_cls_preds = torch.sigmoid(rcnn_cls_preds)
 
         # ----------- REG_VALID_MASK -----------
         reg_fg_thresh = self.model_cfg.TARGET_CONFIG.UNLABELED_REG_FG_THRESH
         filtering_mask = (rcnn_cls_preds > reg_fg_thresh) & (rcnn_cls_labels > reg_fg_thresh)
-        self.forward_ret_dict['reg_valid_mask'][unlabeled_inds] = filtering_mask.long()
+        self.forward_ret_dict['reg_valid_mask'] = filtering_mask.long()
 
         # ----------- RCNN_CLS_LABELS -----------
-        fg_mask = rcnn_cls_labels > self.model_cfg.TARGET_CONFIG.UNLABELED_CLS_FG_THRESH
-        bg_mask = rcnn_cls_labels < self.model_cfg.TARGET_CONFIG.UNLABELED_CLS_BG_THRESH
+        fg_mask = rcnn_cls_labels_ul > self.model_cfg.TARGET_CONFIG.UNLABELED_CLS_FG_THRESH
+        bg_mask = rcnn_cls_labels_ul < self.model_cfg.TARGET_CONFIG.UNLABELED_CLS_BG_THRESH
         ignore_mask = torch.eq(self.forward_ret_dict['gt_of_rois'][unlabeled_inds], 0).all(dim=-1)
-        rcnn_cls_labels[fg_mask] = 1
-        rcnn_cls_labels[bg_mask] = 0
-        rcnn_cls_labels[ignore_mask] = -1
-        self.forward_ret_dict['rcnn_cls_labels'][unlabeled_inds] = rcnn_cls_labels
+        rcnn_cls_labels_ul[fg_mask] = 1
+        rcnn_cls_labels_ul[bg_mask] = 0
+        rcnn_cls_labels_ul[ignore_mask] = -1
+        self.forward_ret_dict['rcnn_cls_labels'][unlabeled_inds] = rcnn_cls_labels_ul
 
     def get_loss(self, tb_dict=None, scalar=True):
         tb_dict = {} if tb_dict is None else tb_dict
